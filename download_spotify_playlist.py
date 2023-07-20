803896ef78c04e49a1f10cd6aad6867af7b3e185
Nikhil-35 <105065237+Nikhil-35@users.noreply.github.com>
2023-07-21 02:53:05 +0530

    comments

--- download_spotify_playlist.py
+++ download_spotify_playlist.py
@@ -5,7 +5,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from googleapiclient.discovery import build
 from pytube import YouTube
 
-# birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 client_credentials_manager = SpotifyClientCredentials(client_id= "YOUR_ID", client_secret="YOUR_SECRET")
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
@@ -15,7 +14,7 @@ playlist_URI = playlist_link.split("/")[-1].split("?")[0]
 track_names = [x["track"]["name"] for x in spotify.playlist_tracks(playlist_URI)["items"]]
 artists_names = [x["track"]["artists"][0]["name"] for x in spotify.playlist_tracks(playlist_URI)["items"]]
 
-
+# names and artists name of the songs in playlist
 res_list = [track_names[i] + " " +artists_names[i] for i in range(len(track_names))]
 res_list = [i for i in res_list if i !=' ']
 # print(res_list)
@@ -25,6 +24,7 @@ fin_size = len(res_list)
 youtube_api_key = "YOUR_YOUTUBE_API_KEY"
 youtube = build('youtube','v3',developerKey=youtube_api_key)
 
+#youtube video links of the songs
 vid_urls = []
 
 for i in range(fin_size):
@@ -41,7 +41,7 @@ for i in range(fin_size):
 for link in vid_urls:
     yt = YouTube(link)
     video = yt.streams.filter(only_audio=True).first()
-    download_file = video.download(output_path='./playlist')
+    download_file = video.download(output_path='./playlist')  #files uploaded to playlist folder in same directory
 
     base, ext = os.path.splitext(download_file)
     new_file = base + '.mp3'
